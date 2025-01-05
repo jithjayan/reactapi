691fbf70bffefc92dc328ccae7a72020df5fb6ff
jithjayan <jithjayan9341@gmail.com>
2025-01-06 00:36:15 +0530

    new

---
+++ reactprjt/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+
+# Create your views here.
+class Task_view(viewsets.ModelViewSet):
+    queryset=Task.objects.all()
+    serializer_class=Model_ser
