f6a57f9e671aff75bc666c60d044e4436f36de52
jithjayan <jithjayan9341@gmail.com>
2025-01-08 09:01:41 +0530

    edit button

--- front/src/components/TaskList.jsx
+++ front/src/components/TaskList.jsx
@@ -3,15 +3,22 @@ import axios from 'axios'
 
 function TaskList(){
     const [data,setData]=useState([])
+    const [editing,setEditing]=useState(false)
+    const [editDtls,setEditDtls]=useState({id:'',title:'',dis:''})
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/api/task/').then(res=>{
             console.log(res.data);
             setData(res.data)
         }).catch(error=>console.log(error.message))
     },[])
+    const edittask =(task)=>{
+        setEditing(true)
+        setEditDtls(task)
+    }
     return(
         <>
-        <table>
+        <div className="container">
+        <table className="table">
             <thead>
                 <tr>
                     <th>Task</th>
@@ -23,13 +30,30 @@ function TaskList(){
                     <tr key={index}>
                         <td>{value.title}</td>
                         <td>{value.dis}</td>
-                        <td><button onClick={()=>{}} class="btn btn-secondary">Edit</button></td>
-                        <td><button onClick={()=>{}} class="btn btn-danger">Delete</button></td>
+                        <td><button onClick={()=>{edittask(value)}} className="btn btn-secondary">Edit</button></td>
+                        <td><button onClick={()=>{}} className="btn btn-danger">Delete</button></td>
                     </tr>
                 ))}
             </tbody>
         </table>
+        {editing ? <EditForm curTask={editDtls}/>:null}
+        </div>
         </>
     )
 }
+
+const EditForm=({curTask})=>{
+    console.log('EditForm',curTask);
+    const [task,setTask]=useState(curTask)
+    return(
+        <>
+        <form >
+            <input type="text" name="task" id="task" value={task.title}/>
+            <input type="text" name="dis" id="dis" value={task.dis}/>
+            <input type="submit" value="update" />
+        </form>
+        </>
+    )
+}
+
 export default TaskList
